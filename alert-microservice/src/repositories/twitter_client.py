7c9ebbd6efc967caae397bbefbc1e56bad323a92
Serene <s.lobo@student.unsw.edu.au>
2026-03-12 20:45:16 +1100

    feat(alert): update client to send weekly queries to bypass twitter data limits -incomplete

--- alert-microservice/src/repositories/twitter_client.py
+++ alert-microservice/src/repositories/twitter_client.py
@@ -18,15 +18,20 @@ BASE_URL = "https://api.twitterapi.io/twitter/tweet/advanced_search"
 
 # Search query focusing on T1 Sydney Trains account for specific disruption keywords.
 # Excludes retweets to minimize duplicate alert data. 
-QUERY = "(from:T1SydneyTrains) (delay OR disruption OR cancelled OR suspended OR delayed OR allow extra time) until:2026-03-11 since:2026-03-01"
 
-def search_tweets(cursor: str = None, last_min_id: str = None) -> dict:
+# Base query (without dates)
+BASE_QUERY = "(from:T1SydneyTrains) (delay OR disruption OR cancelled OR suspended OR delayed OR allow extra time)"
+
+# File where the data will be output 
+JSON_FILE = "historical_t1_tweets.json"
+
+
+def search_tweets_by_query(query: str) -> list:
     """
     Fetches the latest tweets matching the transit disruption query.
 
     Args:
-        last_min_id: the ID of the last tweet returned by cursor to allow retrival of historic data beyond the twitter api limits 
-        cursor: A pagination token used to retrieve the next page of search results.
+        query: updated query with dates for each week 
 
     Returns:
         dict: The JSON response containing tweet data and pagination metadata.
@@ -36,22 +41,29 @@ def search_tweets(cursor: str = None, last_min_id: str = None) -> dict:
     """
 
     headers = {"x-api-key": API_KEY}
+    all_tweets = []
+    seen_ids = set()
+    cursor = None
+    max_retries = 3
+
+    while True: 
     #use max_id to retrive older tweet beyond pagination 
-    params = {
-        "query": QUERY,
-        "queryType": "Latest"
-    }
+        params = {
+            "query": query,
+            "queryType": "Latest"
+        }
 
-    if cursor:
-        params["cursor"] = cursor
-    elif last_min_id:
-        params["query"] = f"{QUERY} max_id:{last_min_id}"
-        
+        if cursor:
+            params["cursor"] = cursor
 
-    # Perform the GET request to the advanced search endpoint
-    response = requests.get(BASE_URL, headers=headers, params=params)
+        retry_count = 0
 
-    # Automatically raises an exception for 4xx or 5xx status codes
-    response.raise_for_status()
+        while retry_count < max_retries:
+            try:
+                # Perform the GET request to the advanced search endpoint
+                response = requests.get(BASE_URL, headers=headers, params=params)
+            except requests.exceptions.RequestException as e:
+                # Automatically raises an exception for 4xx or 5xx status codes
+                response.raise_for_status()
     
     return response.json()
