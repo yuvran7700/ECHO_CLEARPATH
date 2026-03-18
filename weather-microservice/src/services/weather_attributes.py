913339979d9f290c7c8379afa74c2fbe0e881c7b
Alys <alys.weiss@student.unsw.edu.au>
2026-03-18 17:34:29 +1100

    Refactoring

--- weather-microservice/src/services/weather_attributes.py
+++ weather-microservice/src/services/weather_attributes.py
@@ -2,81 +2,29 @@ from decimal import Decimal
 
 from src.repositories.db_repo import put_record
 from src.repositories.s3_repo import read_file
-
-
-def temperature_classification(
-    tempMin: int, tempMax: int, amTemp: int, pmTemp: int
-):
-    avgTemp = (tempMin + tempMax + amTemp + pmTemp) / 4
-
-    if avgTemp < 15:
-        return "Cold"
-    elif avgTemp < 22:
-        return "Mild"
-    elif avgTemp < 30:
-        return "Warm"
-    else:
-        return "Hot"
-
-
-def rainfall_classification(rainfall: int):
-    if rainfall == 0.0:
-        return "No rain"
-    elif rainfall < 10:
-        return "Light rain"
-    elif rainfall < 25:
-        return "Moderate rain"
-    else:
-        return "Heavy rain"
-
-
-def sunshine_classification(sunshineHours: int):
-    sun_ratio = sunshineHours / 24
-    if sun_ratio < 0.4:
-        return "Cloudy"
-    elif sun_ratio <= 0.8:
-        return "Partly Cloudy"
-    else:
-        return "Sunny"
-
-
-def wind_classification(windGustSpeed: int):
-    if windGustSpeed < 20:
-        return "Calm"
-    elif windGustSpeed < 38:
-        return "Breezy"
-    elif windGustSpeed < 61:
-        return "Windy"
-    else:
-        return "Gale"
-
-
-def humidity_classification(amHumidity: int, pmHumidity: int):
-    avgHumidity = (amHumidity + pmHumidity) / 2
-
-    if avgHumidity <= 30:
-        return "Low Humidity"
-    elif avgHumidity <= 60:
-        return "Moderate Humidity"
-    elif avgHumidity <= 80:
-        return "High Humidity"
-    else:
-        return "Extreme Humidity"
+from src.utils.weather_utils import (
+    humidity_classification,
+    rainfall_classification,
+    sunshine_classification,
+    temperature_classification,
+    wind_classification,
+)
 
 
 def process_collected_s3_object(key: str, eTag: str):
     content = read_file(key)
+    attri = content["events"][0]["event_attributes"]
 
-    date = content["events"][0]["event_attributes"]["date"]
-    tempMin = content["events"][0]["event_attributes"]["tempMin"]
-    tempMax = content["events"][0]["event_attributes"]["tempMax"]
-    rainfall = content["events"][0]["event_attributes"]["rainfall"]
-    sunshineHours = content["events"][0]["event_attributes"]["sunshineHours"]
-    windGustSpeed = content["events"][0]["event_attributes"]["windGustSpeed"]
-    amTemp = content["events"][0]["event_attributes"]["9am"]["temp"]
-    amHumidity = content["events"][0]["event_attributes"]["9am"]["humidity"]
-    pmTemp = content["events"][0]["event_attributes"]["3pm"]["temp"]
-    pmHumidity = content["events"][0]["event_attributes"]["3pm"]["humidity"]
+    date = attri["date"]
+    tempMin = attri["tempMin"]
+    tempMax = attri["tempMax"]
+    rainfall = attri["rainfall"]
+    sunshineHours = attri["sunshineHours"]
+    windGustSpeed = attri["windGustSpeed"]
+    amTemp = attri["temp"]
+    amHumidity = attri["humidity"]
+    pmTemp = attri["temp"]
+    pmHumidity = attri["humidity"]
 
     temp_severity = temperature_classification(
         tempMin, tempMax, amTemp, pmTemp
--- weather-microservice/src/utils/weather_utils.py
+++ weather-microservice/src/utils/weather_utils.py
@@ -1,6 +1,66 @@
 from decimal import Decimal
 
 
+def temperature_classification(
+    tempMin: int, tempMax: int, amTemp: int, pmTemp: int
+):
+    avgTemp = (tempMin + tempMax + amTemp + pmTemp) / 4
+
+    if avgTemp < 15:
+        return "Cold"
+    elif avgTemp < 22:
+        return "Mild"
+    elif avgTemp < 30:
+        return "Warm"
+    else:
+        return "Hot"
+
+
+def rainfall_classification(rainfall: int):
+    if rainfall == 0.0:
+        return "No rain"
+    elif rainfall < 10:
+        return "Light rain"
+    elif rainfall < 25:
+        return "Moderate rain"
+    else:
+        return "Heavy rain"
+
+
+def sunshine_classification(sunshineHours: int):
+    sun_ratio = sunshineHours / 24
+    if sun_ratio < 0.4:
+        return "Cloudy"
+    elif sun_ratio <= 0.8:
+        return "Partly Cloudy"
+    else:
+        return "Sunny"
+
+
+def wind_classification(windGustSpeed: int):
+    if windGustSpeed < 20:
+        return "Calm"
+    elif windGustSpeed < 38:
+        return "Breezy"
+    elif windGustSpeed < 61:
+        return "Windy"
+    else:
+        return "Gale"
+
+
+def humidity_classification(amHumidity: int, pmHumidity: int):
+    avgHumidity = (amHumidity + pmHumidity) / 2
+
+    if avgHumidity <= 30:
+        return "Low Humidity"
+    elif avgHumidity <= 60:
+        return "Moderate Humidity"
+    elif avgHumidity <= 80:
+        return "High Humidity"
+    else:
+        return "Extreme Humidity"
+
+
 def decimal_converter(x):
     if isinstance(x, Decimal):
         return float(x)
