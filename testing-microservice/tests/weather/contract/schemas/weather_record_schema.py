53ca94f11d61f0ef0f9ab3991987ee18f21f2cdc
drfee1g00d <140126496+drfee1g00d@users.noreply.github.com>
2026-04-02 00:47:06 +1100

    Tests/weather/contract (#23)
    * Develop/weather microservice (#5)
    
    * feat(infra): add staging/prod SAM config and templates
    
    * feat(infra): add dynamoDb and s3 names into env for SAM
    
    * feat(infra): Add manual gate for prod release
    
    * ci: add AWS credentials to weather integration tests
    
    * feat(infra-alert): add staging/prod SAM config and templates (#6)
    
    * feat(testing): add ecosystem testing setup (#7)
    
    * feat: add testing-microservice skeleton with pytest structure
    
    - introduce testing-microservice module
    - organise tests by service (weather, alert, transport)
    - add contract, api, and smoke test folders
    - add base conftest with environment-aware SSM config
    - add pytest config and requirements
    
    * feat(ci/cd): integrate staging black-box tests into microservice CD pipelines
    
    * feat(ci/cd): add production tests post-deployment
    
    * feat: add requirements.text
    
    * fix: flatten weather attributes before persisting to DynamoDB
    
    * feat: add script to dynamically generate CloudWatch dashboards
    
    * fix: update prod CD to only use smoke tests
    
    * fix: update tests for flattened dynamoDB schema
    
    * Update prod-cd.yml (#8)
    
    * Fix/prod cd pytest args (#9)
    
    * Update prod-cd.yml
    
    * Update prod-cd.yml
    
    * fix: Update prod-cd.yml to always generate test report
    
    * Small changes made to error messages while doing component testing
    
    * Finished all component testing
    
    * Added contract testing
    
    ---------
    
    Co-authored-by: yuvran7700 <alice.walterlobo@gmail.com>
    Co-authored-by: Alys <alys.weiss@student.unsw.edu.au>

---
+++ testing-microservice/tests/weather/contract/schemas/weather_record_schema.py
@@ -0,0 +1,131 @@
+WEATHER_RECORD_SCHEMA = {
+    "type": "object",
+    "required": [
+        "data_source",
+        "dataset_type",
+        "dataset_id",
+        "events",
+    ],
+    "properties": {
+        "data_source": {"type": "string"},
+        "dataset_type": {"type": "string"},
+        "dataset_id": {"type": "string"},
+        "events": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "required": ["event_type", "event_time", "event_attributes"],
+                "properties": {
+                    "event_type": {"type": "string"},
+                    "event_time": {
+                        "type": "object",
+                        "required": [
+                            "time_stamp",
+                            "duration",
+                            "duration_unit",
+                            "time_zone",
+                        ],
+                        "properties": {
+                            "time_stamp": {"type": "string"},
+                            "duration": {"type": "integer"},
+                            "duration_unit": {"type": "string"},
+                            "time_zone": {"type": "string"},
+                        },
+                    },
+                    "event_attributes": {
+                        "type": "object",
+                        "properties": {
+                            "date": {"type": "string"},
+                            "tempMin_C": {
+                                "oneOf": [
+                                    {"type": "number", "format": "float"},
+                                    {"type": "string"},
+                                ]
+                            },
+                            "tempMax_C": {
+                                "oneOf": [
+                                    {"type": "number", "format": "float"},
+                                    {"type": "string"},
+                                ]
+                            },
+                            "rainfall_mm": {
+                                "oneOf": [
+                                    {"type": "number", "format": "float"},
+                                    {"type": "string"},
+                                ]
+                            },
+                            "sunshineHours_hours": {
+                                "oneOf": [
+                                    {"type": "number", "format": "float"},
+                                    {"type": "string"},
+                                ]
+                            },
+                            "maxWindSpeed_kmh": {
+                                "oneOf": [
+                                    {"type": "number", "format": "float"},
+                                    {"type": "string"},
+                                ]
+                            },
+                            "9am": {
+                                "type": "object",
+                                "properties": {
+                                    "temp_C": {
+                                        "oneOf": [
+                                            {
+                                                "type": "number",
+                                                "format": "float",
+                                            },
+                                            {"type": "string"},
+                                        ]
+                                    },
+                                    "humidity_percent": {
+                                        "oneOf": [
+                                            {
+                                                "type": "number",
+                                                "format": "float",
+                                            },
+                                            {"type": "string"},
+                                        ]
+                                    },
+                                },
+                            },
+                            "3pm": {
+                                "type": "object",
+                                "properties": {
+                                    "temp_C": {
+                                        "oneOf": [
+                                            {
+                                                "type": "number",
+                                                "format": "float",
+                                            },
+                                            {"type": "string"},
+                                        ]
+                                    },
+                                    "humidity_percent": {
+                                        "oneOf": [
+                                            {
+                                                "type": "number",
+                                                "format": "float",
+                                            },
+                                            {"type": "string"},
+                                        ]
+                                    },
+                                },
+                            },
+                            "weatherSeverity": {
+                                "type": "object",
+                                "properties": {
+                                    "sunSeverity": {"type": "string"},
+                                    "rainSeverity": {"type": "string"},
+                                    "tempSeverity": {"type": "string"},
+                                    "windSeverity": {"type": "string"},
+                                    "humiditySeverity": {"type": "string"},
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    },
+}
